e46b5eb2e34ac20a4ac9c945e9beea235e2b0501
akhomich <akhomich@WS7-2HQ2482>
2017-06-14 10:59:01 -0400

    Add project files.

---
+++ AOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AOS.Startup))]
+namespace AOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
